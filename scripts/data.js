8fd6559545c785ca603e6969c9e8be561b325fc6
Ignacio Iglesias <ignacioiglesias8@gmail.com>
2023-05-05 01:21:15 -0300

    creando repo

--- scripts/data.js
+++ scripts/data.js
@@ -1,49 +1,71 @@
 const sites = [{
-    name: "Statue of Liberty",
-    continent: "america",
-    country: "unitedStates",
-    region: "New York",
-    image: "https://via.placeholder.com/400x250?text=Statue+of+Liberty",
-    description: "The Statue of Liberty is a colossal neoclassical sculpture on Liberty Island in New York Harbor, in the United States.",
-},
-{
-    name: "Eiffel Tower",
-    continent: "europe",
-    country: "france",
-    region: "Ile-de-France",
-    image: "https://via.placeholder.com/400x250?text=Eiffel+Tower",
-    description: "The Eiffel Tower is a wrought-iron lattice tower on the Champ de Mars in Paris, France.",
-},
-{
-    name: "Taj Mahal",
-    continent: "asia",
-    country: "india",
-    region: "Uttar Pradesh",
-    image: "https://via.placeholder.com/400x250?text=Taj+Mahal",
-    description: "The Taj Mahal is an ivory-white marble mausoleum on the right bank of the Yamuna river in the Indian city of Agra.",
-},
-{
-    name: "Macchu Picchu",
-    continent: "america",
-    country: "peru",
-    region: "Cuzco",
-    image: "https://via.placeholder.com/400x250?text=Macchu+Picchu",
-    description: "The Statue of Liberty is a colossal neoclassical sculpture on Liberty Island in New York Harbor, in the United States.",
-},
-{
-    name: "Cristo Redentor",
-    continent: "america",
-    country: "brazil",
-    region: "Rio do Janeiro",
-    image: "https://via.placeholder.com/400x250?text=Cristo+Redentor",
-    description: "The Eiffel Tower is a wrought-iron lattice tower on the Champ de Mars in Paris, France.",
-},
-{
-    name: "Chichen Itza",
-    continent: "america",
-    country: "mexico",
-    region: "Yucatan",
-    image: "https://via.placeholder.com/400x250?text=Chichen+Itza",
-    description: "The Taj Mahal is an ivory-white marble mausoleum on the right bank of the Yamuna river in the Indian city of Agra.",
-},
+        name: "Statue of Liberty",
+        continent: "America",
+        country: "United States",
+        region: "New York",
+        image: "https://via.placeholder.com/400x250?text=Statue+of+Liberty",
+        description: "The Statue of Liberty is a colossal neoclassical sculpture on Liberty Island in New York Harbor, in the United States.",
+    },
+    {
+        name: "Eiffel Tower",
+        continent: "Europe",
+        country: "France",
+        region: "Ile-de-France",
+        image: "https://via.placeholder.com/400x250?text=Eiffel+Tower",
+        description: "The Eiffel Tower is a wrought-iron lattice tower on the Champ de Mars in Paris, France.",
+    },
+    {
+        name: "Taj Mahal",
+        continent: "Asia",
+        country: "India",
+        region: "Uttar Pradesh",
+        image: "https://via.placeholder.com/400x250?text=Taj+Mahal",
+        description: "The Taj Mahal is an ivory-white marble mausoleum on the right bank of the Yamuna river in the Indian city of Agra.",
+    },
+    {
+        name: "Macchu Picchu",
+        continent: "America",
+        country: "Peru",
+        region: "Cuzco",
+        image: "https://via.placeholder.com/400x250?text=Macchu+Picchu",
+        description: "The Statue of Liberty is a colossal neoclassical sculpture on Liberty Island in New York Harbor, in the United States.",
+    },
+    {
+        name: "Cristo Redentor",
+        continent: "America",
+        country: "Brazil",
+        region: "Rio do Janeiro",
+        image: "https://via.placeholder.com/400x250?text=Cristo+Redentor",
+        description: "The Eiffel Tower is a wrought-iron lattice tower on the Champ de Mars in Paris, France.",
+    },
+    {
+        name: "Chichen Itza",
+        continent: "America",
+        country: "Mexico",
+        region: "Yucatan",
+        image: "https://via.placeholder.com/400x250?text=Chichen+Itza",
+        description: "The Taj Mahal is an ivory-white marble mausoleum on the right bank of the Yamuna river in the Indian city of Agra.",
+    },
+];
+
+const continents = [{
+        name: "America",
+        countries: ["USA", "Canada", "Mexico", "Peru", "Brazil"]
+    },
+    {
+        name: "Europe",
+        countries: ["France", "Spain", "Italy", "Germany", "Greece"]
+    },
+    {
+        name: "Asia",
+        countries: ["China", "Japan", "India", "Thailand", "South Korea"]
+    },
+    {
+        name: "Africa",
+        countries: ["Egypt", "Kenya", "Morocco", "South Africa", "Tanzania"]
+    },
+    {
+        name: "Oceania",
+        countries: ["Australia", "New Zealand", "Fiji", "Papua New Guinea", "Vanuatu"]
+    }
 ];
--- scripts/script.js
+++ scripts/script.js
@@ -1,73 +1,71 @@
-// Define a function to generate a card for each tourist site
-function generateCards() {
-    const cardRow = document.getElementById("card-row");
-    for (let i = 0; i < sites.length; i++) {
-        const site = sites[i];
-        const card = document.createElement("div");
-        card.classList.add("col-md-4");
-        const html = `
-            <div class="card mb-4 box-shadow">
-            <img class="card-img-top" src="${site.image}" alt="${site.name}">
-            <div class="card-body">
-                <h3 class="row card-title justify-content-center">${site.name}</h3>
-                <p class="card-text text-justify">${site.description}</p>
-            </div>
-            </div>`;
-        card.innerHTML = html;
-        cardRow.appendChild(card);
-    }
+const continentSelect = document.querySelector("#continent-filter");
+const countrySelect = document.querySelector("#country-filter");
+const siteList = document.querySelector("#card-row");
+
+// Define una función para crear una tarjeta de sitio turístico
+function createSiteCard(site) {
+    const card = document.createElement("div");
+    card.classList.add("col-md-4");
+    const html = `
+    <div class="card mb-4 box-shadow">
+    <img class="card-img-top" src="${site.image}" alt="${site.name}">
+    <div class="card-body">
+        <h3 class="row card-title justify-content-center">${site.name}</h3>
+        <p class="card-text text-justify">${site.description}</p>
+    </div>
+    </div>`;
+    card.innerHTML = html;
+    return card;
 }
 
-generateCards();
+// Define una función para agregar los sitios turísticos a la página
+function addSitesToPage(sites) {
+    siteList.innerHTML = "";
+    sites.forEach((site) => {
+        const card = createSiteCard(site);
+        siteList.appendChild(card);
+    });
+}
 
-const continentSelect = document.querySelector("#continent-filter");
+// Define una función para crear la lista de países
+function createCountryList(sites) {
+    const countries = [];
+    sites.forEach((site) => {
+        if (!countries.includes(site.country)) {
+            countries.push(site.country);
+        }
+    });
+    countrySelect.innerHTML = `<option value="all-countries">All Countries</option>`;
+    countries.forEach((country) => {
+        const option = document.createElement("option");
+        option.value = country;
+        option.textContent = country;
+        countrySelect.appendChild(option);
+    });
+}
+
+// Agrega todos los sitios turísticos a la página al cargar la página
+addSitesToPage(sites);
+
 continentSelect.addEventListener("change", () => {
     const selectedContinent = continentSelect.value;
-    let filteredSites;    
+    let filteredSites;
     if (selectedContinent === "all") {
         filteredSites = sites;
     } else {
-        filteredSites = sites.filter(site => site.continent === selectedContinent);
+        filteredSites = sites.filter((site) => site.continent === selectedContinent);
+        createCountryList(filteredSites);
     }
-    const siteList = document.querySelector("#card-row");
-    siteList.innerHTML = "";
-    filteredSites.forEach(site => {
-        const listItem = document.createElement("div");
-        listItem.classList.add("col-md-4");
-        listItem.innerHTML = `
-        <div class="card mb-4 box-shadow">
-        <img class="card-img-top" src="${site.image}" alt="${site.name}">
-        <div class="card-body">
-            <h3 class="row card-title justify-content-center">${site.name}</h3>
-            <p class="card-text text-justify">${site.description}</p>
-        </div>
-        </div>`;
-        siteList.appendChild(listItem);
-    });
+    addSitesToPage(filteredSites);
 });
 
-const countrySelect = document.querySelector("#country-filter");
 countrySelect.addEventListener("change", () => {
     const selectedCountry = countrySelect.value;
-    let filteredSites;    
+    let filteredSites;
     if (selectedCountry === "all") {
         filteredSites = sites;
     } else {
-        filteredSites = sites.filter(site => site.country === selectedCountry);
+        filteredSites = sites.filter((site) => site.country === selectedCountry);
     }
-    const siteList = document.querySelector("#card-row");
-    siteList.innerHTML = "";
-    filteredSites.forEach(site => {
-        const listItem = document.createElement("div");
-        listItem.classList.add("col-md-4");
-        listItem.innerHTML = `
-        <div class="card mb-4 box-shadow">
-        <img class="card-img-top" src="${site.image}" alt="${site.name}">
-        <div class="card-body">
-            <h3 class="row card-title justify-content-center">${site.name}</h3>
-            <p class="card-text text-justify">${site.description}</p>
-        </div>
-        </div>`;
-        siteList.appendChild(listItem);
-    });
+    addSitesToPage(filteredSites);
 });
